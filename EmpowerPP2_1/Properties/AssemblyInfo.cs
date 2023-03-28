d2b01aafcef37d02c266f7843d1bf6a362dc5663
eapsuzp <eapsuzp@NLL10ZM6D3>
2023-03-28 16:44:40 +0200

    Change3

---
+++ EmpowerPP2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerPP2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerPP2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7A81C58C-5D4C-4B68-8ABA-8E4D0A394048")]
+[assembly: AssemblyVersion("1.0.0.0")]
